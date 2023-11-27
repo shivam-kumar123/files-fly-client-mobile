f39ce9edc153cb47002e113c211372ef5c4c0dd1
shivam kumar <20051252@kiit.ac.in>
2023-11-27 15:40:30 +0530

    notification and alert beautified

--- components/DownloadInput.tsx
+++ components/DownloadInput.tsx
@@ -1,132 +1,168 @@
+import React, { useState, useEffect } from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    TextInput,
-    Button,
-    Linking,
-    PermissionsAndroid,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  PermissionsAndroid,
+  ToastAndroid,
 } from 'react-native';
-import { useState } from 'react';
 import RNFetchBlob from 'rn-fetch-blob';
 
 type Props = {
-    isDarkMode: boolean
-}
+  isDarkMode: boolean;
+};
 
-export const DownloadInput = ({isDarkMode}: Props) => {
+export const DownloadInput = ({ isDarkMode }: Props) => {
+  const [inputCode, setInputCode] = useState<string>('');
+  const [errorMsg, setErrorMsg] = useState<string>('');
 
-    const [inputCode, setInputCode] = useState<string>('');
-    const [errorMsg, setErrorMsg] = useState<string>('');
-    
-    const requestStoragePermission = async () => {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            {
-              title: 'Files Fly Storage Permission',
-              message:
-                'This App needs access to your Storage ' +
-                'to Download Files',
-              buttonNegative: 'Cancel',
-              buttonPositive: 'OK',
-            },
-          );
-        } catch (err) {
-          console.warn(err);
-        }
-    };
-
-    const handleDownloadFile = async () => {
-        setInputCode('');
-        if(inputCode.length !== 6) {
-            setErrorMsg('Enter a Valid Code');
-        } else {
-            await requestStoragePermission();
-            const fileURL = `https://files-fly-server-mobile.onrender.com/download/${inputCode}`;
-            const { config, fs } = RNFetchBlob;
-            const date = new Date();
-            try {
-                const filePath = fs.dirs.DownloadDir;
-                config({
-                fileCache: true,
-                addAndroidDownloads: {
-                    useDownloadManager: true,
-                    notification: true,
-                    path: filePath + "/download_" + Math.floor(date.getDate() + date.getSeconds()),
-                    description: 'File Download',
-                },
-                }).fetch('GET', fileURL)
-                    .then((res) => {
-                        alert("File Downloaded Successfully");
-                    });
-                // The file is now downloaded, and you can use `res.path()` to get the local path.
-            } catch (error) {
-                console.error('Error downloading file:', error);
-            }
-        }
+  const requestStoragePermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: 'Files Fly Storage Permission',
+          message:
+            'This App needs access to your Storage ' +
+            'to Download Files',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+            ToastAndroid.showWithGravity(
+                'Storage permission denied. Please grant permission to download files.',
+            ToastAndroid.LONG,
+            ToastAndroid.CENTER
+            );
+      }
+    } catch (err) {
+      console.warn(err);
     }
+  };
 
-    const handleInputCode = (text: string) => {
-        setErrorMsg('');
-        setInputCode(text);
-    };    
+  const handleDownloadFile = async () => {
+    setInputCode('');
+    if (inputCode.length !== 6) {
+      setErrorMsg('Enter a Valid Code');
+    } else {
+      await requestStoragePermission();
+      const fileURL = `https://files-fly-server-mobile.onrender.com/download/${inputCode}`;
+      const { config, fs } = RNFetchBlob;
+      const date = new Date();
+      try {
+        ToastAndroid.showWithGravity(
+            'File downloaded Started!',
+            ToastAndroid.LONG,
+            ToastAndroid.TOP
+          );
+        const filePath = fs.dirs.DownloadDir;
 
-    return (
-        <View>
+        const res = await config({
+          fileCache: true,
+          addAndroidDownloads: {
+            useDownloadManager: true,
+            notification: true,
+            title: 'File Download',
+            path: filePath + '/download_' + Math.floor(date.getDate() + date.getSeconds()) + '.ext',
+            description: 'File Download',
+          },
+        }).fetch('GET', fileURL);
 
-            <Text style={[styles.text, {
-                color: isDarkMode ? 'white' : 'black'
-            }]}>Download File</Text>
+        // console.log('File downloaded to:', res.path());
 
-            <TextInput 
-                style={[styles.input, {
-                    color: isDarkMode ? 'white' : 'black'
-                }]}
-                onChangeText={handleInputCode}
-                placeholder="Enter 6-digit Code"
-                placeholderTextColor={isDarkMode ? 'white' : "#000"}
-                value={inputCode}
-            />
+        // Show a success message to the user
+        ToastAndroid.showWithGravity(
+          'File downloaded successfully!',
+          ToastAndroid.LONG,
+          ToastAndroid.CENTER
+        );
+      } catch (error) {
+        console.error('Error downloading file:', error);
 
-            <View style={styles.btn} >
-                <Button
-                    title='Download File'
-                    onPress={handleDownloadFile}
-                />
-            </View>
+        // Show an error message to the user
+        ToastAndroid.showWithGravity(
+          'Failed to download file. Please try again.',
+          ToastAndroid.LONG,
+          ToastAndroid.CENTER
+        );
+      }
+    }
+  };
 
+  const handleInputCode = (text: string) => {
+    setErrorMsg('');
+    setInputCode(text);
+  };
+
+  return (
+    <View>
+      <Text
+        style={[
+          styles.text,
+          {
+            color: isDarkMode ? 'white' : 'black',
+          },
+        ]}
+      >
+        Download File
+      </Text>
+
+      <TextInput
+        style={[
+          styles.input,
+          {
+            color: isDarkMode ? 'white' : 'black',
+          },
+        ]}
+        onChangeText={handleInputCode}
+        placeholder="Enter 6-digit Code"
+        placeholderTextColor={isDarkMode ? 'white' : '#000'}
+        value={inputCode}
+      />
+
+      <View style={styles.btn}>
+        <Button title="Download File" onPress={handleDownloadFile} />
+      </View>
+
+      {errorMsg !== '' && (
+        <Text
+          style={[
+            styles.err,
             {
-                errorMsg !== '' && 
-                <Text style={[styles.err, {
-                    color: isDarkMode ? 'white' : 'black'
-                }]}>{errorMsg}</Text>
-            }
-
-        </View>
-  )
+              color: isDarkMode ? 'white' : 'black',
+            },
+          ]}
+        >
+          {errorMsg}
+        </Text>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 22,
-        marginTop: 50,
-        textAlign: 'center',
-    },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-      },
-      btn: {
-        marginTop: 25,
-        marginLeft: 50,
-        marginRight: 50,
-    },
-    err: {
-        fontSize: 22,
-        textAlign: 'center',
-        marginTop: 50,
-    }
+  text: {
+    fontSize: 22,
+    marginTop: 50,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  btn: {
+    marginTop: 25,
+    marginLeft: 50,
+    marginRight: 50,
+  },
+  err: {
+    fontSize: 22,
+    textAlign: 'center',
+    marginTop: 50,
+  },
 });
