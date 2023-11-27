8b50821e648d632798bdc8657ff83ec047918d32
shivam kumar <20051252@kiit.ac.in>
2023-11-27 14:00:15 +0530

    storage permission added with download alert

--- components/DownloadInput.tsx
+++ components/DownloadInput.tsx
@@ -4,9 +4,11 @@ import {
     StyleSheet,
     TextInput,
     Button,
-    Linking
+    Linking,
+    PermissionsAndroid,
 } from 'react-native';
 import { useState } from 'react';
+import RNFetchBlob from 'rn-fetch-blob';
 
 type Props = {
     isDarkMode: boolean
@@ -17,14 +19,52 @@ export const DownloadInput = ({isDarkMode}: Props) => {
     const [inputCode, setInputCode] = useState<string>('');
     const [errorMsg, setErrorMsg] = useState<string>('');
     
+    const requestStoragePermission = async () => {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            {
+              title: 'Files Fly Storage Permission',
+              message:
+                'This App needs access to your Storage ' +
+                'to Download Files',
+              buttonNegative: 'Cancel',
+              buttonPositive: 'OK',
+            },
+          );
+        } catch (err) {
+          console.warn(err);
+        }
+    };
+
     const handleDownloadFile = async () => {
         setInputCode('');
         if(inputCode.length !== 6) {
             setErrorMsg('Enter a Valid Code');
         } else {
+            await requestStoragePermission();
             const fileURL = `https://files-fly-server-mobile.onrender.com/download/${inputCode}`;
-            Linking.openURL(fileURL).catch((err) => console.error('Error opening URL:', err));
-        };
+            const { config, fs } = RNFetchBlob;
+            const date = new Date();
+            try {
+                const filePath = fs.dirs.DownloadDir;
+                config({
+                fileCache: true,
+                addAndroidDownloads: {
+                    useDownloadManager: true,
+                    notification: true,
+                    path: filePath + "/download_" + Math.floor(date.getDate() + date.getSeconds()),
+                    description: 'File Download',
+                },
+                }).fetch('GET', fileURL)
+                    .then((res) => {
+                        alert("File Downloaded Successfully");
+                    });
+                // The file is now downloaded, and you can use `res.path()` to get the local path.
+            } catch (error) {
+                console.error('Error downloading file:', error);
+            }
+        }
     }
 
     const handleInputCode = (text: string) => {
